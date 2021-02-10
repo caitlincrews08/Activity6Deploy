8267f283e5dece1bf1d75aa968fd78ad1be6a3a1
Caitlin Crews <caitlinrcrews@gmail.com>
2021-02-09 21:56:44 -0500

    included production info

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
 {
